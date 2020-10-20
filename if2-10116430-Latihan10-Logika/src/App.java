6be0e0e9dfe765decf59cc9f041df2ffc313df58
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:43:34 +0700

    Add files via upload

---
+++ if2-10116430-Latihan10-Logika/src/App.java
@@ -0,0 +1,14 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
